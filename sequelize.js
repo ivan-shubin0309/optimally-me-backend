ab4316986794aa7b8976bdabb7bf8722557ae81c
ivan-shubin0309 <ivan.shubin0309@gmail.com>
2022-09-06 22:35:51 +0300

    sequelize migrate

--- sequelize.js
+++ sequelize.js
@@ -1,18 +1,12 @@
 const dotenv = require("dotenv");
 const fs = require("fs");
 const path = require("path");
-const sequelize = dotenv.parse(fs.readFileSync(path.resolve(`.env.${process.env.NODE_ENV}`)));
+const config = dotenv.parse(fs.readFileSync(path.resolve(`.env.${process.env.NODE_ENV}`)));
 
 module.exports = {
-    "host": sequelize.DB_HOST,
-    "port": sequelize.DB_PORT,
-    "database": sequelize.DB_NAME,
-    "username": sequelize.DB_USER,
-    "password": sequelize.DB_PASSWORD,
-    "charset": "utf8mb4",
-    "dialect": "mysql",
-    "connectionRetryCount": 5,
-    "maxConnections": 10,
-    "delayBeforeReconnect": 3000,
-    "showErrors": true,
+    "host": config.DB_HOST,
+    "port": config.DB_PORT,
+    "database": config.DB_NAME,
+    "username": config.DB_USER,
+    "password": config.DB_PASSWORD,
 }
