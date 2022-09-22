03dbf1d9c5b346d98f5425f2dedb4d1108ad0eaa
ivan-shubin0309 <ivan.shubin0309@gmail.com>
2022-09-22 15:30:03 +0300

    OD-13 [BL] US-19 As a Super Admin I want to choose Category/Panel so that I can apply specific category to each biomarker
    *** fix
    
    clean unused code

--- apps/biomarkers/src/biomarkers.controller.ts
+++ apps/biomarkers/src/biomarkers.controller.ts
@@ -1,18 +1,9 @@
-import {
-  Get,
-  Post,
-  Body,
-  Controller,
-  Request
-} from '@nestjs/common';
+import { Get, Controller } from '@nestjs/common';
 import { ApiBearerAuth, ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Roles } from '../../common/src/resources/common/role.decorator';
-import { CreateBiomarkerDto } from './models';
-import { SessionDto } from '../../sessions/src/models';
 import { UserRoles } from '../../common/src/resources/users';
 import { BiomarkersService } from './biomarkers.service';
 import { CategoriesDto } from './models';
-import { TranslatorService } from 'nestjs-translator';
 
 @ApiBearerAuth()
 @ApiTags('biomarkers')
