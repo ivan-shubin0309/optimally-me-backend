039f9b6169454f174515566e3e275f126d293c6a
ivan-shubin0309 <ivan.shubin0309@gmail.com>
2022-09-22 17:45:23 +0300

    OD-13 [BL] US-19 As a Super Admin I want to choose Category/Panel so that I can apply specific category to each biomarker
    ** fix
    
    By Dymas comment

--- apps/biomarkers/src/biomarkers.service.ts
+++ apps/biomarkers/src/biomarkers.service.ts
@@ -1,21 +1,22 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { Repository } from 'sequelize-typescript';
 import { Category } from './models';
 
 @Injectable()
 export class BiomarkersService {
   constructor(
-    @Inject('CATEGORY_MODEL') private readonly categoriesModel: typeof Category,
+    @Inject('CATEGORY_MODEL') private readonly categoryModel: Repository<Category>,
   ) {}
 
 
   getListCategories(scopes = []):  Promise<Category[]> {
-    return this.categoriesModel
+    return this.categoryModel
       .scope(scopes)
       .findAll();
   }
 
   getCategoriesCount(scopes = []):  Promise<number> {
-    return this.categoriesModel
+    return this.categoryModel
       .scope(scopes)
       .count();
   }
