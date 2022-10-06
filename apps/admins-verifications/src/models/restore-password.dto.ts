e725b3b42429b55d746d8278b02d80ea13f6e0a6
ivan-shubin0309 <ivan.shubin0309@gmail.com>
2022-10-06 17:27:04 +0300

    OD-166 Error message dont match specification
    **fix**
    - returned to default error messages for validations

--- apps/admins-verifications/src/models/restore-password.dto.ts
+++ apps/admins-verifications/src/models/restore-password.dto.ts
@@ -1,14 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsInt, IsNotEmpty, IsOptional, IsPositive } from 'class-validator';
-import { INVALID_EMAIL_ERROR_MESSAGE } from '../../../common/src/resources/users';
 
 export class RestorePasswordDto {
 
     @ApiProperty({ type: () => String, required: true })
     @IsNotEmpty()
-    @IsEmail({},{
-        message: INVALID_EMAIL_ERROR_MESSAGE
-    })
+    @IsEmail()
     readonly email: string;
 
     @ApiProperty({ type: () => Number, required: false })
