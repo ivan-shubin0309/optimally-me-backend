f58d89e8b63c0f56acf6cbeee70d697f98d00014
ivan-shubin0309 <ivan.shubin0309@gmail.com>
2022-10-19 16:17:57 +0300

    OD-291
    **features**
    - added Perimenopause to filter other features

--- apps/common/src/resources/filters/other-feature-types.ts
+++ apps/common/src/resources/filters/other-feature-types.ts
@@ -1,9 +1,11 @@
 export enum OtherFeatureTypes {
     pregnant = 1,
     menopause = 2,
+    perimenopause = 3
 }
 
 export const OtherFeatureClientValues = {
     [OtherFeatureTypes.pregnant]: 'Pregnant',
-    [OtherFeatureTypes.menopause]: 'Menopause'
+    [OtherFeatureTypes.menopause]: 'Menopause',
+    [OtherFeatureTypes.perimenopause]: 'Perimenopause',
 };
