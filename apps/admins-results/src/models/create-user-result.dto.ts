959fb0d972f82a70f7e51b7f9d80e8c06bfdf1e1
ivan-shubin0309 <ivan.shubin0309@gmail.com>
2022-10-19 16:21:30 +0300

    OD-293 Admin can add result with value which consist of more then 10 symbols
    **fix**
    - added validation for value field in CreateUserResultDto

--- apps/admins-results/src/models/create-user-result.dto.ts
+++ apps/admins-results/src/models/create-user-result.dto.ts
@@ -1,7 +1,8 @@
-import { IsNotEmpty, IsNumber, IsInt, IsPositive } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsInt, IsPositive, Max } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { IsOnlyDate } from '../../../common/src/resources/common/is-only-date.decorator';
 import { IsDateInPast } from '../../../common/src/resources/common/is-date-in-past.decorator';
+import { resultsValidationRules } from '../../../common/src/resources/results/results-validation-rules';
 
 export class CreateUserResultDto {
     @ApiProperty({ type: () => Number, required: true })
@@ -13,6 +14,7 @@ export class CreateUserResultDto {
     @ApiProperty({ type: () => Number, required: true })
     @IsNotEmpty()
     @IsNumber()
+    @Max(resultsValidationRules.maxValue)
     readonly value: number;
 
     @ApiProperty({ type: () => String, required: true })
