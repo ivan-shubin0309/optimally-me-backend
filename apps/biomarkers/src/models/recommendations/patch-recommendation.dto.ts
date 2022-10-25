18953d7467b610b7c2fa558bdb826dad29762768
ivan-shubin0309 <ivan.shubin0309@gmail.com>
2022-10-25 18:11:46 +0300

    OD-260 US-112 As a Super Admin I want to archive the recommendation
    **features**
    - added api for marking recommendation as archived

---
+++ apps/biomarkers/src/models/recommendations/patch-recommendation.dto.ts
@@ -0,0 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsNotEmpty } from 'class-validator';
+
+export class PatchRecommendationDto {
+    @ApiProperty({ type: () => Boolean })
+    @IsNotEmpty()
+    @IsBoolean()
+    isArchived: boolean;
+}
