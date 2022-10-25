dea37c47efa4a0b8bfdc618c84508f323e53d431
ivan-shubin0309 <ivan.shubin0309@gmail.com>
2022-10-25 13:11:05 +0300

    OD-289 Create API for Android SDK, Samsung Health, Apple Health
    **fix**
    - fixed boolean parsing when deleting wefitter connection

--- apps/wefitter/src/models/delete-connection.dto.ts
+++ apps/wefitter/src/models/delete-connection.dto.ts
@@ -1,7 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { nonWefitterConnectionSlugs } from '../../../common/src/resources/wefitter/non-wefitter-connection-slugs';
 import { Type } from 'class-transformer';
-import { IsNotEmpty } from 'class-validator';
+import { IsBoolean, IsNotEmpty } from 'class-validator';
+import { ParseBoolean } from 'apps/common/src/resources/common/parse-boolean.decorator';
 
 export class DeleteConnectionDto {
     @ApiProperty({ type: () => String, required: true, description: nonWefitterConnectionSlugs.join(', ') })
@@ -11,11 +12,13 @@ export class DeleteConnectionDto {
 
     @ApiProperty({ type: () => Boolean, required: true, default: false })
     @IsNotEmpty()
-    @Type(() => Boolean)
+    @IsBoolean()
+    @ParseBoolean()
     readonly deleteData: boolean;
 
     @ApiProperty({ type: () => Boolean, required: true, default: true })
     @IsNotEmpty()
-    @Type(() => Boolean)
+    @IsBoolean()
+    @ParseBoolean()
     readonly isWefitterConnectionSlug: boolean = true;
 }
