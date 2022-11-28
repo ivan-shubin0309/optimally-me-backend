df7d97caa28345090048702437a4bef2b8a611ff
ivan-shubin0309 <ivan.shubin0309@gmail.com>
2022-11-28 15:18:18 +0200

    OD-544 US-207 Logic for biomarker’s sorting on the Heat map
    **fix**
    - reworked diviation calculation
    - reworked sorting by diviation

--- apps/common/src/resources/usersBiomarkers/user-biomarker-range-types.ts
+++ apps/common/src/resources/usersBiomarkers/user-biomarker-range-types.ts
@@ -1,5 +1,4 @@
 import { RecommendationTypes } from '../recommendations/recommendation-types';
-import { BASE_DEVIATION_STEP } from './constants';
 
 export enum UserBiomarkerRangeTypes {
     critical = 1,
@@ -15,13 +14,4 @@ export const recommendationTypesToRangeTypes = {
     [RecommendationTypes.low]: UserBiomarkerRangeTypes.bad,
     [RecommendationTypes.subOptimal]: UserBiomarkerRangeTypes.borderline,
     [RecommendationTypes.supraOptimal]: UserBiomarkerRangeTypes.borderline,
-};
-
-export const baseDeviation = {
-    [RecommendationTypes.criticalLow]: BASE_DEVIATION_STEP * 3,
-    [RecommendationTypes.criticalHigh]: BASE_DEVIATION_STEP * 3,
-    [RecommendationTypes.high]: BASE_DEVIATION_STEP * 2,
-    [RecommendationTypes.low]: BASE_DEVIATION_STEP * 2,
-    [RecommendationTypes.subOptimal]: BASE_DEVIATION_STEP,
-    [RecommendationTypes.supraOptimal]: BASE_DEVIATION_STEP,
 };
