ff4ed8abc42393737f70799c69ad086103aa9733
ivan-shubin0309 <ivan.shubin0309@gmail.com>
2022-12-06 11:20:36 +0200

    OD-595
    **fix**
    - reworked shopify integration using axios

--- apps/shopify/src/shopify.service.ts
+++ apps/shopify/src/shopify.service.ts
@@ -1,24 +1,30 @@
 import { HttpStatus, Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { ConfigService } from '../../common/src/utils/config/config.service';
-import * as ShopifyAPI from 'shopify-node-api';
 import { User } from '../../users/src/models';
 import { UpdateShopifyCustomerDto } from './models/update-shopify-customer.dto';
 import * as crypto from 'crypto';
+import axios from 'axios';
 
 const apiVersion = '2022-10';
 
+type Record<K extends keyof any, T> = {
+  [P in K]: T;
+};
+
 @Injectable()
 export class ShopifyService {
-  private readonly shopifyApi: ShopifyAPI;
+  private readonly baseUrl: string;
   constructor(
     private readonly configService: ConfigService,
   ) {
-    this.shopifyApi = new ShopifyAPI({
-      shop: configService.get('SHOPIFY_IDP_IDENTIFIER'),
-      shopify_api_key: configService.get('SHOPIFY_API_KEY'),
-      shopify_shared_secret: configService.get('SHOPIFY_WEBHOOK_SECRET'),
-      access_token: configService.get('SHOPIFY_API_ACCESS_TOKEN')
-    });
+    this.baseUrl = `https://${configService.get('SHOPIFY_IDP_IDENTIFIER')}.myshopify.com`;
+  }
+
+  private getHeaders(): Record<string, string | number | boolean> {
+    return {
+      'Content-Type': 'application/json',
+      'X-Shopify-Access-Token': this.configService.get('SHOPIFY_API_ACCESS_TOKEN')
+    };
   }
 
   verifyWebhook(hmac: string, rawBody: Buffer): boolean {
@@ -34,25 +40,18 @@ export class ShopifyService {
   }
 
   async updateCustomer(customerId: string, user: User): Promise<void> {
-    const url = `/admin/api/${apiVersion}/customers/${customerId}.json`;
+    const url = `${this.baseUrl}/admin/api/${apiVersion}/customers/${customerId}.json`;
     const data = { customer: new UpdateShopifyCustomerDto(user) };
 
-    const promise = new Promise((resolve, reject) =>
-      this.shopifyApi.put(url, data, function (err, data) {
-        if (err) {
-          return reject(err);
-        }
-        return resolve(data);
-      })
-    );
-
-    await promise.catch(err => {
+    try {
+      await axios.put(url, data, { headers: this.getHeaders() });
+    } catch (err) {
       console.log(err.message);
       throw new UnprocessableEntityException({
         message: err.message,
         errorCode: 'SHOPIFY_REQUEST_ERROR',
         statusCode: HttpStatus.UNPROCESSABLE_ENTITY
       });
-    });
+    }
   }
 }
