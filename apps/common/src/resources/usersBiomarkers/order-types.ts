f6890abe6e77e9d10c24ce648f69a2c939401a40
ivan-shubin0309 <ivan.shubin0309@gmail.com>
2023-01-16 17:46:16 +0200

    OD-916  Sorting by date on the "Blood biomarkers" page should work according to this rule: New blood biomarkers shall be sorted initially -  1) from newest to oldest, 2) then from Critical to Optimal
    **fix**
    - fixed date sorting

--- apps/common/src/resources/usersBiomarkers/order-types.ts
+++ apps/common/src/resources/usersBiomarkers/order-types.ts
@@ -1,5 +1,6 @@
 import { GetUserBiomarkersListDto } from '../../../../users-biomarkers/src/models/get-user-biomarkers-list.dto';
 import sequelize from 'sequelize';
+import { RecommendationTypes } from '../recommendations/recommendation-types';
 
 export const userBiomarkersOrderTypes = [
     'deviation',
@@ -12,5 +13,24 @@ export const userBiomarkerOrderScope = {
     'deviation': (query: GetUserBiomarkersListDto) => ({ method: ['orderByDeviation', query.orderType] }),
     'category': (query: GetUserBiomarkersListDto) => ({ method: ['orderBy', [[sequelize.literal('`category.name`'), query.orderType], ['name', query.orderType]]] }),
     'name': (query: GetUserBiomarkersListDto) => ({ method: ['orderBy', [['name', query.orderType]]] }),
-    'date': (query: GetUserBiomarkersListDto) => ({ method: ['orderBy', [[sequelize.literal('`lastResult`.`date`'), query.orderType], ['name', query.orderType]]] }),
+    'date': (query: GetUserBiomarkersListDto) => ({
+        method: [
+            'orderBy',
+            [
+                [sequelize.literal('`lastResult`.`date`'), query.orderType],
+                sequelize.literal(`FIELD(\`lastResult\`.\`recommendationRange\`, ${recommendationRangeSort.join(',')}) ${query.orderType === 'desc' ? 'asc' : 'desc'}`),
+                [sequelize.literal('`lastResult`.`deviation`'), query.orderType],
+            ]
+        ]
+    }),
 };
+
+const recommendationRangeSort = [
+    RecommendationTypes.criticalHigh,
+    RecommendationTypes.criticalLow,
+    RecommendationTypes.high,
+    RecommendationTypes.low,
+    RecommendationTypes.supraOptimal,
+    RecommendationTypes.subOptimal,
+    RecommendationTypes.optimal,
+];
