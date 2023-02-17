d31e9ca9d0ac765478308639fa54d7a95cde7dd5
ivan-shubin0309 <ivan.shubin0309@gmail.com>
2023-02-17 17:57:53 +0200

    OD-1173 Create Lambda by cron which will send push notification to Firebase
    **features**
    - added check when customer has connected Connection: Android, Apple Health SDK, Samsung SDK

--- apps/users-devices/src/models/user-device.entity.ts
+++ apps/users-devices/src/models/user-device.entity.ts
@@ -1,10 +1,33 @@
-import { Table, Column, Model, DataType, ForeignKey, Scopes } from 'sequelize-typescript';
+import { UserWefitter } from '../../../wefitter/src/models/user-wefitter.entity';
+import { Table, Column, Model, DataType, ForeignKey, Scopes, BelongsTo } from 'sequelize-typescript';
 import { User } from '../../../users/src/models';
+import { Op } from 'sequelize';
 
 @Scopes(() => ({
     byId: (id) => ({ where: { id } }),
     bySessionId: (sessionId) => ({ where: { sessionId } }),
     pagination: (query) => ({ limit: query.limit, offset: query.offset }),
+    withConnectedSDK: () => ({
+        include: [
+            {
+                model: User,
+                as: 'user',
+                required: true,
+                include: [{
+                    model: UserWefitter,
+                    as: 'wefitter',
+                    required: true,
+                    where: {
+                        [Op.or]: [
+                            { isAppleHealthConnected: true },
+                            { isSamsungHealthConnected: true },
+                            { isAndroidSdkConnected: true },
+                        ]
+                    }
+                }]
+            }
+        ]
+    }),
 }))
 @Table({
     tableName: 'userDevices',
@@ -30,4 +53,7 @@ export class UserDevice extends Model {
         allowNull: false,
     })
     sessionId: string;
+
+    @BelongsTo(() => User, 'userId')
+    user: User;
 }
