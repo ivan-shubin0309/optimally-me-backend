36fbcf6b20d67c7910c64b9624a2b575478a9e7c
ivan-shubin0309 <ivan.shubin0309@gmail.com>
2023-02-18 16:24:08 +0200

    OD-1186 On Sign up page user can't create password with Special Symbols
    **fix**
    - added new rules to password

--- apps/common/src/resources/users/validation-rules.ts
+++ apps/common/src/resources/users/validation-rules.ts
@@ -9,7 +9,7 @@ export const UsersValidationRules = {
     ageMinValue: 18
 };
 
-export const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+export const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d!@#$%^&(){}\[\]:;<>,.?\/~_+-=|\\]{8,}$/;
 
 export const PASSWORD_ERROR_MESSAGE = 'should contain at least 1 upper-case letter and 1 digit, spaces are not allowed.';
 
