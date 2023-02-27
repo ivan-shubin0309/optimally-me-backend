4667d5d28b9ec480ec0e1eccaf558a9faee54d94
ivan-shubin0309 <ivan.shubin0309@gmail.com>
2023-02-27 14:51:31 +0200

    OD-1294 Changes for Orange color filtering (Dashboard heatmap)
    **fix**
    - fixed dashboard counter

--- apps/users-biomarkers/src/users-biomarkers.service.ts
+++ apps/users-biomarkers/src/users-biomarkers.service.ts
@@ -30,7 +30,7 @@ export class UsersBiomarkersService extends BaseService<Biomarker> {
         results.forEach(result => {
             //eslint-disable-next-line @typescript-eslint/ban-ts-comment
             //@ts-ignore
-            const rangeType = recommendationTypesToRangeTypes[result.get('recommendationRangeValue')];
+            const rangeType = recommendationTypesToRangeTypes[result.get('recommendationRange')];
             const fieldName = UserBiomarkerRangeTypes[rangeType];
             if (rangeType && fieldName) {
                 if (!resultMap[fieldName]) {
