c113bb4be6f578460707c3d448e738d15b4ea873
ivan-shubin0309 <ivan.shubin0309@gmail.com>
2023-03-13 15:13:38 +0200

    [WIP]OD-1332

--- apps/hl7/src/hl7-ftp.service.ts
+++ apps/hl7/src/hl7-ftp.service.ts
@@ -1,7 +1,7 @@
 import { HttpStatus, Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { ConfigService } from '../../common/src/utils/config/config.service';
-import * as ftp from 'basic-ftp';
 import axios from 'axios';
+import * as ClientSftp from 'ssh2-sftp-client'; 
 
 const HL7_BASE_REQUEST_PATH = 'HL7/HL7 Requests';
 
@@ -11,25 +11,23 @@ export class Hl7FtpService {
         private readonly configService: ConfigService,
     ) { }
 
-    private async getFtpClient(): Promise<ftp.Client> {
-        const client = new ftp.Client();
-        client.ftp.verbose = true;
-        try {
-            await client.access({
+    private async getFtpClient(): Promise<ClientSftp> {
+        const client = new ClientSftp();
+        await client
+            .connect({
                 host: this.configService.get('HL7_FTP_HOST'),
                 port: this.configService.get('HL7_FTP_PORT'),
-                user: this.configService.get('HL7_FTP_USERNAME'),
+                username: this.configService.get('HL7_FTP_USERNAME'),
                 password: this.configService.get('HL7_FTP_PASSWORD'),
-                secure: true
+            })
+            .catch(err => {
+                console.log(err.message);
+                throw new UnprocessableEntityException({
+                    message: err.message,
+                    errorCode: 'HL7_FTP_CONNECTION_ERROR',
+                    statusCode: HttpStatus.UNPROCESSABLE_ENTITY
+                });
             });
-        } catch (err) {
-            console.log(err);
-            throw new UnprocessableEntityException({
-                message: err.message,
-                errorCode: 'HL7_FTP_CONNECTION_ERROR',
-                statusCode: HttpStatus.UNPROCESSABLE_ENTITY
-            });
-        }
         return client;
     }
 
@@ -40,7 +38,7 @@ export class Hl7FtpService {
                 { headers: { responseType: 'stream' } }
             )
             .catch(err => {
-                console.log(err);
+                console.log(err.message);
                 throw new UnprocessableEntityException({
                     message: err.message,
                     errorCode: 'HL7_S3_FILE_ERROR',
@@ -52,16 +50,14 @@ export class Hl7FtpService {
         const client = await this.getFtpClient();
 
         await client
-            .uploadFrom(fileStream, `${HL7_BASE_REQUEST_PATH}/${fileName}.hl7`)
+            .put(fileStream, `${HL7_BASE_REQUEST_PATH}/${fileName}.hl7`)
             .catch(err => {
-                console.log(err);
+                console.log(err.message);
                 throw new UnprocessableEntityException({
                     message: err.message,
                     errorCode: 'HL7_FTP_UPLOAD_ERROR',
                     statusCode: HttpStatus.UNPROCESSABLE_ENTITY
                 });
             });
-
-        client.close();
     }
 }
