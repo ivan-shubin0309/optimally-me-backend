ad9241071b8673b9f3f14c6ef678c93eb346df71
ivan-shubin0309 <ivan.shubin0309@gmail.com>
2023-04-03 16:53:56 +0300

    Od-1393 US-454 As a Super Admin I want to have updated detailed view with the tag
    **fix**
    - added missing scopes when getting recommendation tags

--- apps/biomarkers/src/models/recommendationTags/recommendation-tag.entity.ts
+++ apps/biomarkers/src/models/recommendationTags/recommendation-tag.entity.ts
@@ -1,18 +1,19 @@
 import { Table, Column, Model, DataType, Scopes, ForeignKey } from 'sequelize-typescript';
 import { Recommendation } from '../recommendations/recommendation.entity';
-import { fn, col } from 'sequelize';
+import { fn, col, Op } from 'sequelize';
 
 @Scopes(() => ({
     byId: (id) => ({ where: { id } }),
     byRecommendationId: (recommendationId) => ({ where: { recommendationId } }),
-    distinctDates: () => ({
-        attributes: [[fn('DISTINCT', col('date')), 'date']],
+    distinctNames: () => ({
+        attributes: [[fn('DISTINCT', col('name')), 'name']],
     }),
-    distinctDatesCount: () => ({
-        attributes: [[fn('COUNT', fn('DISTINCT', col('date'))), 'counter']],
+    distinctNamesCount: () => ({
+        attributes: [[fn('COUNT', fn('DISTINCT', col('name'))), 'counter']],
     }),
     orderBy: (arrayOfOrders: [[string, string]]) => ({ order: arrayOfOrders }),
     pagination: (query) => ({ limit: query.limit, offset: query.offset }),
+    search: (searchString) => ({ where: { name: { [Op.like]: `%${searchString}%` } } }),
 }))
 @Table({
     tableName: 'recommendationTags',
