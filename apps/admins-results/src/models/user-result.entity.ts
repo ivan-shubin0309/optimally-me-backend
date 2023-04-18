3cfb1def6b09b231d87a39b02de13fcc93f7110e
ivan-shubin0309 <ivan.shubin0309@gmail.com>
2023-04-18 14:11:19 +0300

    OD-1781 500 internal server Error appears when API result-dates is used on Dasboard
    **fix**
    - removed attributes from scope byBiomarkerType for userResults entity

--- apps/admins-results/src/models/user-result.entity.ts
+++ apps/admins-results/src/models/user-result.entity.ts
@@ -131,7 +131,8 @@ export interface IUserResult {
                 required: true,
                 where: {
                     type: biomarkerType
-                }
+                },
+                attributes: []
             },
         ]
     })
