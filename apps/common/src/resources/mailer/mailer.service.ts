3d721ba77aee1170a9fcae0843533d44e77e4022
ivan-shubin0309 <ivan.shubin0309@gmail.com>
2023-05-03 15:19:31 +0300

    OD-1654 Update email template
    **features**
    - rewritten mailer service
    - created script for template upload
    - changed template for emails

--- apps/common/src/resources/mailer/mailer.service.ts
+++ apps/common/src/resources/mailer/mailer.service.ts
@@ -36,6 +36,7 @@ export class MailerService {
     }
 
     private async sendMail(params: IEmailParams): Promise<void> {
+        let response;
         const emailParams = new SesMessageParamsDto(
             params.from || this.configService.get('SES_SOURCE_EMAIL'),
             params.to,
@@ -56,68 +57,49 @@ export class MailerService {
         );
 
         if (params.templateName) {
-            await this.SES.sendTemplatedEmail(emailParams);
+            response = await this.SES.sendTemplatedEmail(emailParams);
         } else {
-            await this.SES.sendEmail(emailParams);
+            response = await this.SES.sendEmail(emailParams);
         }
-    }
 
-
-    private async sendEmail(html: string, subject: string, sendToEmail: string): Promise<void> {
-
-        const params = {
-            Destination: {
-                ToAddresses: [sendToEmail]
-            },
-            Message: {
-                Body: {
-                    Html: {
-                        Charset: 'UTF-8',
-                        Data: html
-                    },
-                },
-                Subject: {
-                    Charset: 'UTF-8',
-                    Data: subject
-                }
-            },
-            Source: this.configService.get('SES_SOURCE_EMAIL'),
-        };
-
-        const command = new SendEmailCommand(params);
-        const response = await this.SES.send(command);
         console.log('MailerService: Email sent with SES');
         console.log(JSON.stringify(response));
     }
 
     async sendRestorePasswordEmail(user: User, token: string): Promise<void> {
-
-        return this.sendEmail(
-            this.translatorService.translate('RESTORE_PASSWORD_EMAIL_TEXT', {
-                replace: {
-                    link: `${this.configService.get('ADMIN_FRONTEND_BASE_URL')}/auth/restore-password?code=${token}`,
-                    firstName: user.firstName,
-                    lastName: user.lastName
-                }
-            }),
-            this.translatorService.translate('RESTORE_PASSWORD_EMAIL_SUBJECT'),
-            user.email
-        );
+        return this.sendMail({
+            to: [user.email],
+            templateData: {
+                title: this.translatorService.translate('RESTORE_PASSWORD_EMAIL_SUBJECT'),
+                body: this.translatorService.translate('RESTORE_PASSWORD_EMAIL_TEXT', {
+                    replace: {
+                        link: `${this.configService.get('ADMIN_FRONTEND_BASE_URL')}/auth/restore-password?code=${token}`,
+                        firstName: user.firstName,
+                        lastName: user.lastName
+                    }
+                }),
+                subject: this.translatorService.translate('RESTORE_PASSWORD_EMAIL_SUBJECT'),
+            },
+            templateName: EmailTemplates.BASE_EMAIL_TEMPLATE.NAME,
+        });
     }
 
     async sendUserRestorePasswordEmail(user: User, link: string): Promise<void> {
-
-        return this.sendEmail(
-            this.translatorService.translate('USER_RESTORE_PASSWORD_EMAIL_TEXT', {
-                replace: {
-                    link,
-                    firstName: user.firstName,
-                    lastName: user.lastName
-                }
-            }),
-            this.translatorService.translate('USER_RESTORE_PASSWORD_EMAIL_SUBJECT'),
-            user.email
-        );
+        return this.sendMail({
+            to: [user.email],
+            templateData: {
+                title: this.translatorService.translate('USER_RESTORE_PASSWORD_EMAIL_SUBJECT'),
+                body: this.translatorService.translate('USER_RESTORE_PASSWORD_EMAIL_TEXT', {
+                    replace: {
+                        link,
+                        firstName: user.firstName,
+                        lastName: user.lastName
+                    }
+                }),
+                subject: this.translatorService.translate('USER_RESTORE_PASSWORD_EMAIL_SUBJECT')
+            },
+            templateName: EmailTemplates.BASE_EMAIL_TEMPLATE.NAME,
+        });
     }
 
     async sendUserVerificationEmail(user: User, token: string, queryString?: string): Promise<void> {
@@ -127,40 +109,43 @@ export class MailerService {
             link = `${link}&${queryString}`;
         }
 
-        return this.sendMail(
-            {
-                to: [user.email],
-                templateData: {
-                    title: this.translatorService.translate('USER_EMAIL_VERIFICATION_SUBJECT'),
-                    body: this.translatorService.translate('USER_EMAIL_VERIFICATION_TEXT', {
-                        replace: { link }
-                    }),
-                    subject: this.translatorService.translate('USER_EMAIL_VERIFICATION_SUBJECT')
-                },
-                templateName: EmailTemplates.BASE_EMAIL_TEMPLATE.NAME,
-            }
-        );
+        return this.sendMail({
+            to: [user.email],
+            templateData: {
+                title: this.translatorService.translate('USER_EMAIL_VERIFICATION_SUBJECT'),
+                body: this.translatorService.translate('USER_EMAIL_VERIFICATION_TEXT', {
+                    replace: { link }
+                }),
+                subject: this.translatorService.translate('USER_EMAIL_VERIFICATION_SUBJECT')
+            },
+            templateName: EmailTemplates.BASE_EMAIL_TEMPLATE.NAME,
+        });
     }
 
     async sendAdminSampleIdError(user: User, options: { sampleId: string, resultAt: string, labName: string, customerId: number | string, rawFile: string }): Promise<void> {
         const link = `${this.configService.get('ADMIN_FRONTEND_BASE_URL')}/app/customer-results`;
 
-        return this.sendEmail(
-            this.translatorService.translate('ADMIN_EMAIL_SAMPLE_ID_ERROR_BODY', {
-                replace:
-                {
-                    sampleId: options.sampleId, 
-                    resultAt: options.resultAt,
-                    labName: options.labName,
-                    customerId: options.customerId.toString(),
-                    rawFile: options.rawFile,
-                    link
-                }
-            }),
-            this.translatorService.translate('ADMIN_EMAIL_SAMPLE_ID_ERROR_SUBJECT', {
-                replace: { sampleId: options.sampleId, resultAt: options.resultAt }
-            }),
-            user.email
-        );
+        return this.sendMail({
+            to: [user.email],
+            templateData: {
+                title: this.translatorService.translate('ADMIN_EMAIL_SAMPLE_ID_ERROR_SUBJECT', {
+                    replace: { sampleId: options.sampleId, resultAt: options.resultAt }
+                }),
+                body: this.translatorService.translate('ADMIN_EMAIL_SAMPLE_ID_ERROR_BODY', {
+                    replace: {
+                        sampleId: options.sampleId,
+                        resultAt: options.resultAt,
+                        labName: options.labName,
+                        customerId: options.customerId.toString(),
+                        rawFile: options.rawFile,
+                        link
+                    }
+                }),
+                    subject: this.translatorService.translate('ADMIN_EMAIL_SAMPLE_ID_ERROR_SUBJECT', {
+                        replace: { sampleId: options.sampleId, resultAt: options.resultAt }
+                    }),
+            },
+            templateName: EmailTemplates.BASE_EMAIL_TEMPLATE.NAME,
+        });
     }
 }
