1d4586d16966d3525555a1d282bb2dcab8becaaf
ivan-shubin0309 <ivan.shubin0309@gmail.com>
2023-05-12 11:27:38 +0300

    OD-1882 US-597 As a Customer I want to view the date and result when I choose date till which the results shall be rolled back
    **features**
    - changed field to filter by when filtering users biomarkers by date

--- apps/common/src/resources/usersBiomarkers/queries.ts
+++ apps/common/src/resources/usersBiomarkers/queries.ts
@@ -1,6 +1,8 @@
 import { RecommendationTypes } from '../recommendations/recommendation-types';
 
 export function getLastUserResultsForEachBiomarker(userId: number, numberOfLastRecords: number, afterDate?: string, beforeDate?: string): string {
+
+
     return `
         SELECT
             \`orderedUserResults\`.\`id\` as \`id\`
@@ -10,8 +12,8 @@ export function getLastUserResultsForEachBiomarker(userId: number, numberOfLastR
                 ROW_NUMBER() OVER (PARTITION BY \`userResults\`.\`biomarkerId\` ORDER BY \`userResults\`.\`date\` DESC, \`userResults\`.\`createdAt\` DESC) as \`orderValue\`        
             FROM \`userResults\`
             WHERE \`userResults\`.\`userId\`=${userId}
-            ${beforeDate ? `AND \`userResults\`.\`date\`<='${beforeDate}'` : ''}
-            ${afterDate ? `AND \`userResults\`.\`date\`>='${afterDate}'` : ''}
+            ${beforeDate ? `AND IF(\`userResults\`.\`skinUserResultId\` IS NOT NULL, \`userResults\`.\`createdAt\`, \`userResults\`.\`date\`)<='${beforeDate}'` : ''}
+            ${afterDate ? `AND IF(\`userResults\`.\`skinUserResultId\` IS NOT NULL, \`userResults\`.\`createdAt\`, \`userResults\`.\`date\`)>='${afterDate}'` : ''}
         ) as \`orderedUserResults\`
         WHERE \`orderedUserResults\`.\`orderValue\` <= ${numberOfLastRecords}
     `.replace(/\s+/ig, ' ').trim();
