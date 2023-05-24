df708e34a1b22167e41cbc53970971a1a532aea1
ivan-shubin0309 <ivan.shubin0309@gmail.com>
2023-05-24 14:16:34 +0300

    OD-2046 US-456.1 As a Customer I want to trigger Klaviyo event when test kit is activated
    **features**
    - changed klaviyo kit activation event name

--- apps/common/src/resources/klaviyo/klaviyo-event-types.ts
+++ apps/common/src/resources/klaviyo/klaviyo-event-types.ts
@@ -1,6 +1,6 @@
 export enum KlaviyoEventTypes {
     accountCreated = 'ACCOUNT_CREATED',
-    kitActivated = 'ACTIVATED_KIT',
+    kitActivated = 'KIT_ACTIVATED',
     sampleReceived = 'SAMPLE_RECEIVED',
     resultsReady = 'RESULTS_READY',
     badResults = 'BAD_RESULTS',
