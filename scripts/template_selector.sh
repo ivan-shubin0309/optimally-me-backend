93b1a2fe95cbc01ae50d39751397053bacd80463
ivan-shubin0309 <ivan.shubin0309@gmail.com>
2023-06-20 10:16:26 +0000

    Update template_selector.sh

--- scripts/template_selector.sh
+++ scripts/template_selector.sh
@@ -3,6 +3,7 @@
 echo "Selecting template for using";
 echo "CONCURENCY UNITS IS "$CONCURENCY_UNITS;
 if [ "$CONCURENCY_UNITS" -le "0" ]
+then
     mv ./templates/template_no_pk.yaml ./templates/template.yaml;
 else
     mv ./templates/template_with_pk.yaml ./templates/template.yaml;
