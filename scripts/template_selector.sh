d30152ed8ec5e659df2dfb6cf523c1490328534a
ivan-shubin0309 <ivan.shubin0309@gmail.com>
2023-06-20 13:01:45 +0300

    Added selector for template

---
+++ scripts/template_selector.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Selecting template for using";
+echo "CONCURENCY UNITS IS "$CONCURENCY_UNITS;
+if [ "$CONCURENCY_UNITS" -le "0" ]
+    mv ./templates/template_no_pk.yaml ./templates/template.yaml;
+else
+    mv ./templates/template_with_pk.yaml ./templates/template.yaml;
+fi
+mv ./templates/template.yaml .;
+echo "Template selected.";
