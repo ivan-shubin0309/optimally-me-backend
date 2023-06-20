70ab4324ffc17ae699eb0dad68db5480b228a1bd
ivan-shubin0309 <ivan.shubin0309@gmail.com>
2023-06-20 12:50:15 +0000

    Update template_selector.sh

--- scripts/template_selector.sh
+++ scripts/template_selector.sh
@@ -5,8 +5,10 @@ echo "CONCURENCY UNITS IS "$CONCURENCY_UNITS;
 if [ "$CONCURENCY_UNITS" -le "0" ]
 then
     mv ./templates/template_no_pk.yaml ./templates/template.yaml;
+    echo "Template without concurency."
 else
     mv ./templates/template_with_pk.yaml ./templates/template.yaml;
+    echo "Template with concurency."
 fi
 mv ./templates/template.yaml .;
 echo "Template selected.";
