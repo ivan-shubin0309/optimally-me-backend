a99af90e613b007ba43b42d2659a821c204c2468
ivan-shubin0309 <ivan.shubin0309@gmail.com>
2023-06-29 15:06:00 +0300

    [WIP]OD-1806

---
+++ migrations/20230629100220-modify-userWefitterSleepSummary-table.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.sequelize.query(`
+            CREATE FUNCTION IF NOT EXISTS DurationToSeconds (dur CHAR(32))
+            RETURNS INTEGER DETERMINISTIC
+            RETURN (CASE
+                WHEN duration LIKE 'P%DT%H%M%.%S' THEN TIME_TO_SEC(STR_TO_DATE(duration, 'P%dDT%HH%iM%s.%fS'))
+                WHEN duration LIKE 'P%DT%H%M%S' THEN TIME_TO_SEC(STR_TO_DATE(duration, 'P%dDT%HH%iM%sS'))
+                WHEN duration LIKE 'PT%H%M%.%S' THEN TIME_TO_SEC(STR_TO_DATE(duration, 'PT%HH%iM%s.%fS'))
+                WHEN duration LIKE 'PT%H%M%S' THEN TIME_TO_SEC(STR_TO_DATE(duration, 'PT%HH%iM%sS'))
+                WHEN duration LIKE 'PT%H%M%' THEN TIME_TO_SEC(STR_TO_DATE(duration, 'PT%HH%iM%'))
+                WHEN duration LIKE 'PT%M%.%S' THEN TIME_TO_SEC(STR_TO_DATE(duration, 'PT%iM%s.%fS'))
+                WHEN duration LIKE 'PT%M%S' THEN TIME_TO_SEC(STR_TO_DATE(duration, 'PT%iM%sS'))
+                WHEN duration LIKE 'PT%.%S' THEN TIME_TO_SEC(STR_TO_DATE(duration, 'PT%s.%fS'))
+                WHEN duration LIKE 'PT%S' THEN TIME_TO_SEC(STR_TO_DATE(duration, 'PT%sS'))
+                WHEN duration LIKE 'PT%M' THEN TIME_TO_SEC(STR_TO_DATE(duration, 'PT%iM%'))
+                WHEN duration LIKE 'P0D' THEN 0
+                WHEN duration LIKE 'PT%H%.%S' THEN TIME_TO_SEC(STR_TO_DATE(duration, 'PT%sHH%.%fS'))
+            END);
+        `);
+    },
+
+    async down(queryInterface, Sequelize) {
+        await queryInterface.sequelize.query(`
+            DROP FUNCTION IF EXISTS DurationToSeconds;
+        `);
+    }
+};
