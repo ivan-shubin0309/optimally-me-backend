2365e98d738d199eb9b1652fdef6689a036d141a
ivan-shubin0309 <ivan.shubin0309@gmail.com>
2023-06-30 11:56:48 +0300

    OD-1806 For the Time asleep parameter, the Min Max values ​​are incorrectly distributed in the table to the graph. On the Dashboard / Biomarker Details / Mirror Mirror
    **fix**
    - fixed DurationToSeconds function

---
+++ migrations/20230630085341-modify-durationToSeconds-function.js
@@ -0,0 +1,52 @@
+'use strict';
+
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.sequelize.query(`
+            DROP FUNCTION IF EXISTS DurationToSeconds;
+        `);
+        await queryInterface.sequelize.query(`
+            CREATE FUNCTION IF NOT EXISTS DurationToSeconds (dur CHAR(32))
+            RETURNS INTEGER DETERMINISTIC
+            RETURN (CASE
+                WHEN dur LIKE 'P%DT%H%M%.%S' THEN TIME_TO_SEC(STR_TO_DATE(dur, 'P%dDT%HH%iM%s.%fS'))
+                WHEN dur LIKE 'P%DT%H%M%S' THEN TIME_TO_SEC(STR_TO_DATE(dur, 'P%dDT%HH%iM%sS'))
+                WHEN dur LIKE 'PT%H%M%S' THEN TIME_TO_SEC(STR_TO_DATE(dur, 'PT%HH%iM%sS'))
+                WHEN dur LIKE 'PT%H%M%.%S' THEN TIME_TO_SEC(STR_TO_DATE(dur, 'PT%HH%iM%s.%fS'))
+                WHEN dur LIKE 'PT%H%.%S' THEN TIME_TO_SEC(STR_TO_DATE(dur, 'PT%kH%s.%fS'))
+                WHEN dur LIKE 'PT%H%M%' THEN TIME_TO_SEC(STR_TO_DATE(dur, 'PT%HH%iM%'))
+                WHEN dur LIKE 'PT%M%.%S' THEN TIME_TO_SEC(STR_TO_DATE(dur, 'PT%iM%s.%fS'))
+                WHEN dur LIKE 'PT%M%S' THEN TIME_TO_SEC(STR_TO_DATE(dur, 'PT%iM%sS'))
+                WHEN dur LIKE 'PT%.%S' THEN TIME_TO_SEC(STR_TO_DATE(dur, 'PT%s.%fS'))
+                WHEN dur LIKE 'PT%S' THEN TIME_TO_SEC(STR_TO_DATE(dur, 'PT%sS'))
+                WHEN dur LIKE 'PT%M' THEN TIME_TO_SEC(STR_TO_DATE(dur, 'PT%iM%'))
+                WHEN dur LIKE 'P0D' THEN 0
+                WHEN dur LIKE 'PT%H%.%S' THEN TIME_TO_SEC(STR_TO_DATE(dur, 'PT%sHH%.%fS'))
+            END);
+        `);
+    },
+
+    async down(queryInterface, Sequelize) {
+        await queryInterface.sequelize.query(`
+            DROP FUNCTION IF EXISTS DurationToSeconds;
+        `);
+        await queryInterface.sequelize.query(`
+            CREATE FUNCTION IF NOT EXISTS DurationToSeconds (dur CHAR(32))
+            RETURNS INTEGER DETERMINISTIC
+            RETURN (CASE
+                WHEN dur LIKE 'P%DT%H%M%.%S' THEN TIME_TO_SEC(STR_TO_DATE(dur, 'P%dDT%HH%iM%s.%fS'))
+                WHEN dur LIKE 'P%DT%H%M%S' THEN TIME_TO_SEC(STR_TO_DATE(dur, 'P%dDT%HH%iM%sS'))
+                WHEN dur LIKE 'PT%H%M%.%S' THEN TIME_TO_SEC(STR_TO_DATE(dur, 'PT%HH%iM%s.%fS'))
+                WHEN dur LIKE 'PT%H%.%S' THEN TIME_TO_SEC(STR_TO_DATE(dur, 'PT%kH%i.%fS'))
+                WHEN dur LIKE 'PT%H%M%' THEN TIME_TO_SEC(STR_TO_DATE(dur, 'PT%HH%iM%'))
+                WHEN dur LIKE 'PT%M%.%S' THEN TIME_TO_SEC(STR_TO_DATE(dur, 'PT%iM%s.%fS'))
+                WHEN dur LIKE 'PT%M%S' THEN TIME_TO_SEC(STR_TO_DATE(dur, 'PT%iM%sS'))
+                WHEN dur LIKE 'PT%.%S' THEN TIME_TO_SEC(STR_TO_DATE(dur, 'PT%s.%fS'))
+                WHEN dur LIKE 'PT%S' THEN TIME_TO_SEC(STR_TO_DATE(dur, 'PT%sS'))
+                WHEN dur LIKE 'PT%M' THEN TIME_TO_SEC(STR_TO_DATE(dur, 'PT%iM%'))
+                WHEN dur LIKE 'P0D' THEN 0
+                WHEN dur LIKE 'PT%H%.%S' THEN TIME_TO_SEC(STR_TO_DATE(dur, 'PT%sHH%.%fS'))
+            END);
+        `);
+    }
+};
